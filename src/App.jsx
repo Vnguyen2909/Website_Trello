f30b435df694026ca092963ee7ecd0c2945c885e
Vnguyen2909 <nguyenvi290903@gmail.com>
2026-03-04 20:11:05 +0700

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import Button from '@mui/material/Button'
 import HomeIcon from '@mui/icons-material/Home'
 import { red } from '@mui/material/colors'
+import Typography from '@mui/material/Typography'
 
 function App() {
 
   return (
     <>
       <div>HELLO WORD</div>
+      <Typography variant='body2' color="text.secondary">Test Typography</Typography>
+
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
       <Button variant="outlined">Outlined</Button>
+      <br />
       <HomeIcon sx={{color: red[500]}}/>
     </>
   )
