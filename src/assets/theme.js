7c64089a0edfc37a985a6f6d2e607ff91f67202f
Vnguyen2909 <nguyenvi290903@gmail.com>
2026-03-04 20:23:10 +0700

    update theme

--- src/assets/theme.js
+++ src/assets/theme.js
@@ -1,11 +1,12 @@
 import { createTheme } from '@mui/material/styles';
 import { red } from '@mui/material/colors';
+import { light } from '@mui/material/styles/createPalette';
 
 // Create a theme instance.
 const theme = createTheme({
   cssVariables: true,
   palette: {
-    mode: 'dark',
+    mode: {light},
     primary: {
       main: '#556cd6',
     },
