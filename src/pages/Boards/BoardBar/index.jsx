b694056f065c9e3f7b7cbae8f5d0cab12fcd1db3
Vnguyen2909 <nguyenvi290903@gmail.com>
2026-03-28 15:49:36 +0700

    fix color theme

--- src/pages/Boards/BoardBar/index.jsx
+++ src/pages/Boards/BoardBar/index.jsx
@@ -12,13 +12,13 @@ import Button  from '@mui/material/Button'
 import PersonAddIcon from '@mui/icons-material/PersonAdd';
 
 const Menu_Style = {
-    color: 'primary.main', 
-    bgcolor: 'white', 
+    color: 'white', 
+    bgcolor: 'transparent', 
     border: 'none', 
     paddingX: '5px', 
     borderRadius: '4px',
     '& .MuiSvgIcon-root': {
-         color: 'primary.main'
+         color: 'white'
     },
     '&:hover': {
         bgcolor: 'primary.50'
@@ -34,7 +34,8 @@ function BoardBar() {
             justifyContent: 'space-between',
             gap: 2,
             overflow: 'auto',
-            borderTop: '1px solid #00bfa5'
+            borderBottom: '1px solid #1752e9',
+            bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#30495e' : '#1976b2')
             }}>
 
             <Box sx={{display: 'flex', alignItems: 'center', gap: 2}}>
@@ -61,13 +62,25 @@ function BoardBar() {
             </Box>
 
             <Box sx={{display: 'flex', alignItems: 'center', gap: 2}}>
-                <Button variant='outlined' startIcon={<PersonAddIcon/>}>Invite</Button>
-                <AvatarGroup max={4} 
+                <Button 
+                    variant='outlined'
+                    startIcon={<PersonAddIcon/>}
+                    sx={{
+                        color: 'white', 
+                        borderColor: 'white',
+                        '&:hover': {borderColor: 'white'}
+                    }}
+                >
+                    Invite
+                </Button>
+                <AvatarGroup max={4}
                     sx={{'& .MuiAvatar-root': {
                         width: 34,
                         height: 34,
-                        fontSize: 16
+                        fontSize: 16,
+                        border: 'none'
                         },
+                        gap : '10px'
                     }}
                 >
                     <Tooltip title="2909nguyenv">
